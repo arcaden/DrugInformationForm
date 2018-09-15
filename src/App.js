c961ce4e50a0993027e899eb1f1b81c6e421b4dd
Glen <glenwu2@gmail.com>
2018-09-15 19:42:50 -0400

    pre eject

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import DrugForm from "./FormInput/DrugForm";
+import Sidebar from "./Navigation/Sidebar/Sidebar";
 
 class App extends Component {
   render() {
     return (
         <div style = {{margin:30}}>
-        <DrugForm/>
+              <Sidebar/>
         </div>
     )
   }
